c3cc890234587d19a86de62d91560af4e4c36aae
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2016-10-23 14:45:50 +0200

    Some script and playbook cleanup

--- rpi-network-setup/initAllRpi.sh
+++ rpi-network-setup/initAllRpi.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo `date +"%d-%m-%y %T"`
-ansible-playbook -i hosts basicSetup.yml
+ansible-playbook -i hosts playbooks/Rpi3basicSetup.yml --limit=rpi3
+ansible-playbook -i hosts playbooks/Rpi2basicSetup.yml --limit=rpi2
 echo `date +"%d-%m-%y %T"`
