87c111dea0c52c35537017321c22772bcce5f99a
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2016-10-31 09:58:22 +0100

    Add proxy support

---
+++ rpi-network-setup/playbooks/roles/useProxy/files/proxy.sh
@@ -0,0 +1,5 @@
+export http_proxy=http://netbook.local:3128
+export https_proxy=http://netbook.local:3128
+export HTTP_PROXY=http://netbook.local:3128
+export HTTPS_PROXY=http://netbook.local:3128
+export no_proxy="localhost,127.0.0.1,.local"
